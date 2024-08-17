e390fa6c1ee09ccfc6ac5b2aa5953b7c551146f8
satyamgupta0704 <21bcs7408@cuchd.in>
2024-08-17 12:07:30 +0530

    inital

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
